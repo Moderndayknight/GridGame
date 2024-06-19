b2085aed556807fe2b75ccd4d5ac3ac4874d26df
Moderndayknight <30375689+Moderndayknight@users.noreply.github.com>
2024-06-18 20:33:18 -0500

    Added 7 levels and finished micro game! Hooray!

--- Assets/_Scripts/LevelGenerator.cs
+++ Assets/_Scripts/LevelGenerator.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-   public Texture2D levelImage;
+   private int _currentLevel;
+   public Texture2D[] _levels;
    public ColorPrefabMapStructure[] colorMappings;
    [SerializeField] GameObject player;
    private GameObject[,] _map;
@@ -10,16 +11,19 @@ public class LevelGenerator : MonoBehaviour
    // Start is called before the first frame update
    void Start()
    {
+      // Start at level 1
+      _currentLevel = 7;
+
       GenerateLevel();
    }
 
    void GenerateLevel()
    {
-      _map = new GameObject[levelImage.width, levelImage.height];
+      _map = new GameObject[_levels[_currentLevel - 1].width, _levels[_currentLevel - 1].height];
 
-      for (int x = 0; x < levelImage.width; ++x)
+      for (int x = 0; x < _levels[_currentLevel - 1].width; ++x)
       {
-         for (int y = 0; y < levelImage.height; ++y)
+         for (int y = 0; y < _levels[_currentLevel - 1].height; ++y)
          {
             _map[x, y] = GenerateTile(x, y);
          }
@@ -30,11 +34,12 @@ public class LevelGenerator : MonoBehaviour
    {
       GameObject generatedObject = new GameObject();
 
-      Color pixelColor = levelImage.GetPixel(x, y);
+      Color pixelColor = _levels[_currentLevel - 1].GetPixel(x, y);
 
       if (pixelColor.a == 0)
       {
          // The pixel is transparent so we ignore it
+         generatedObject.name = "empty";
          return generatedObject;
       }
 
@@ -47,13 +52,13 @@ public class LevelGenerator : MonoBehaviour
             {
                // Move the player to starting position
                player.transform.position = new Vector3(x, y, -1);
-               generatedObject = player;
             }
             else
             {
                // Generate everything else
                Vector3 position = new Vector3(x, y, -1);
                generatedObject = Instantiate(colorMapping._prefab, position, Quaternion.identity, transform);
+               generatedObject.name = string.Format("Object {0} {1}", x, y);
             }
          }
       }
@@ -61,11 +66,11 @@ public class LevelGenerator : MonoBehaviour
       return generatedObject;
    }
 
-   public bool MoveObjects(Vector3 playerPosition, Vector3 movement)
+   public bool MoveObjects(Vector3 oldPosition, Vector3 movement, bool boxMovement = false)
    {
       bool allowable = false;
 
-      Vector3 newPosition = playerPosition + movement;
+      Vector3 newPosition = oldPosition + movement;
 
       int MAP_WIDTH_INDEX = 0;
       int MAP_HEIGHT_INDEX = 1;
@@ -82,9 +87,9 @@ public class LevelGenerator : MonoBehaviour
             return false;
          }
 
-         if (objectInDesiredTile.CompareTag("Box"))
+         if (objectInDesiredTile.CompareTag("Box") && !boxMovement)
          {
-            bool boxCanMove = MoveObjects(objectInDesiredTile.transform.position, movement);
+            bool boxCanMove = MoveObjects(objectInDesiredTile.transform.position, movement, true);
 
             if (!boxCanMove)
             {
@@ -96,7 +101,17 @@ public class LevelGenerator : MonoBehaviour
             _map[(int)newPosition.x, (int)newPosition.y] = new GameObject();
             // Move box to next tile
             _map[(int)objectInDesiredTile.transform.position.x, (int)objectInDesiredTile.transform.position.y] = objectInDesiredTile;
-            
+
+         }
+         else if (objectInDesiredTile.CompareTag("Box"))
+         {
+            // Only allow first iteration to move box (can't push two boxes at once)
+            return false;
+         }
+
+         if (objectInDesiredTile.CompareTag("Goal") && boxMovement)
+         {
+            return false;
          }
 
          bool positionInsideWall = _map[(int)newPosition.x, (int)newPosition.y].CompareTag("Wall");
@@ -106,5 +121,69 @@ public class LevelGenerator : MonoBehaviour
       }
 
       return allowable;
+   }
+
+   private void ClearLevel()
+   {
+      // Delete all the gameObjects
+      for (int i = 0; i < _map.GetLength(0); ++i)
+      {
+         for (int j = 0; j < _map.GetLength(1); ++j)
+         {
+            DestroyImmediate(_map[i, j]);
+         }
+      }
+   }
+
+   public void GenerateLevel(int levelNumber)
+   {
+      if (levelNumber <= 0 || levelNumber > _levels.Length)
+      {
+         Debug.Log("Roll credits");
+
+         // Reset back to first level
+         levelNumber = 1;
+      }
+
+      // Delete all the gameObjects
+      ClearLevel();
+
+      _currentLevel = levelNumber;
+
+      // Generate new empty map
+     _map = new GameObject[_levels[_currentLevel - 1].width, _levels[_currentLevel - 1].height];
+
+      // Load level
+      GenerateLevel();
+   }
+
+   private bool OnGoal(Vector3 position)
+   {
+      return _map[(int)position.x, (int)position.y].CompareTag("Goal");
+   }
+
+   public bool CheckForWin(Vector3 playerPosition)
+   {
+      bool win = false;
+
+      if (OnGoal(playerPosition))
+      {
+         win = true;
+
+         GenerateLevel(_currentLevel + 1);
+      }
+
+      return win;
+   }
+
+   public void Reset()
+   {
+      ClearLevel();
+      GenerateLevel();
+   }
+
+   public int GetCurrentLevel()
+   {
+      return _currentLevel;
    }
 }
--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+   float TICK_TIME_SECS = 0.25f;
+
    [SerializeField] GridManager _gridManager;
    [SerializeField] LevelGenerator _levelGenerator;
    private int _boundaryWidth;
    private int _boundaryHeight;
+   private float _tickTimer;
+   private bool _newTick;
 
    // Start is called before the first frame update
    void Start()
    {
+      _tickTimer = 0;
+      _newTick = false;
       _boundaryWidth = _gridManager.GetWidth();
       _boundaryHeight = _gridManager.GetHeight();
    }
@@ -19,7 +25,13 @@ public class PlayerController : MonoBehaviour
    // Update is called once per frame
    void Update()
    {
-      HandleWasd();
+      _tickTimer += Time.deltaTime;
+
+      // Check for win
+      _levelGenerator.CheckForWin(transform.position);
+
+      // Handle user inputs
+      HandleInputs();
    }
 
    private void Move(Vector3 movement)
@@ -34,23 +46,56 @@ public class PlayerController : MonoBehaviour
       }
    }
 
-   private void HandleWasd()
+   private bool HandleGameTickTimer()
    {
-      if (Input.GetKeyDown(KeyCode.W))
+      bool newTick = _tickTimer > TICK_TIME_SECS;
+
+      if (newTick)
       {
+         // Reset timer if this frame is a new game tick
+         _tickTimer = 0;
+      }
+
+      return newTick;
+   }
+
+   private void HandleInputs()
+   {
+      _newTick = HandleGameTickTimer();
+
+      if (Input.GetKeyDown(KeyCode.W) || (Input.GetKey(KeyCode.W) && _newTick))
+      {
+         _tickTimer = 0.0f;
          Move(new Vector3(0.0f, 1.0f, 0.0f));
       }
-      else if (Input.GetKeyDown(KeyCode.A))
+      else if (Input.GetKeyDown(KeyCode.A) || (Input.GetKey(KeyCode.A) && _newTick))
       {
+         _tickTimer = 0.0f;
          Move(new Vector3(-1.0f, 0.0f, 0.0f));
       }
-      else if (Input.GetKeyDown(KeyCode.S))
+      else if (Input.GetKeyDown(KeyCode.S) || (Input.GetKey(KeyCode.S) && _newTick))
       {
+         _tickTimer = 0.0f;
          Move(new Vector3(0.0f, -1.0f, 0.0f));
       }
-      else if (Input.GetKeyDown(KeyCode.D))
+      else if (Input.GetKeyDown(KeyCode.D) || (Input.GetKey(KeyCode.D) && _newTick))
       {
+         _tickTimer = 0.0f;
          Move(new Vector3(1.0f, 0.0f, 0.0f));
+      }
+      else if (Input.GetKeyDown(KeyCode.R))
+      {
+         _levelGenerator.Reset();
+      }
+      else if (Input.GetKeyDown(KeyCode.KeypadPlus))
+      {
+         Debug.Log("+");
+         _levelGenerator.GenerateLevel(_levelGenerator.GetCurrentLevel() + 1);
+      }
+      else if (Input.GetKeyDown(KeyCode.KeypadMinus))
+      {
+         Debug.Log("-");
+         _levelGenerator.GenerateLevel(_levelGenerator.GetCurrentLevel() - 1);
       }
    }
 
